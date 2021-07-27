d61453fe0a46394cdefcf8d3787e940528c3073b
EC2 Default User <ec2-user@ip-172-31-33-153.ec2.internal>
2021-07-27 14:40:36 +0000

    reko

--- helloworld/application.py
+++ helloworld/application.py
@@ -2,6 +2,7 @@
 import json
 from flask import Flask, Response
 from helloworld.flaskrun import flaskrun
+import boto3
 
 application = Flask(__name__)
 
@@ -12,6 +13,24 @@ def get():
 @application.route('/', methods=['POST'])
 def post():
     return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+
+@application.route('/analyze/<bucket>/<image>', methods=['GET'])
+def analyze(bucket='yakov-my-upload-bucket-01', image='person.jpg'):
+    return detect_labels(bucket, image)
+    
+def detect_labels(bucket, key, max_labels=3, min_confidence=90, region="us-east-1"):
+    rekognition = boto3.client("rekognition", region)
+    s3 = boto3.resource('s3', region_name = 'us-east-1')
+    image = s3.Object(bucket, key) # Get an Image from S3
+    img_data = image.get()['Body'].read() # Read the image
+    response = rekognition.detect_labels(
+        Image={
+            'Bytes': img_data
+        },
+        MaxLabels=max_labels,
+		MinConfidence=min_confidence,
+    )
+    return json.dumps(response['Labels'])
 
 if __name__ == '__main__':
     flaskrun(application)
